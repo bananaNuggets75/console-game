4ae05e8013eb8872d6ec38fe4fce3040030a4b11
bananaNuggets75 <gazokenan@gmail.com>
2024-04-28 19:20:03 +0800

    added class delay

---
+++ hospital_exploration/Delay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace hospital_escape
+{
+    public class Delay
+    {
+        private int delayMilliseconds;
+
+        public Delay(int delayMilliseconds)
+        {
+            this.delayMilliseconds = delayMilliseconds;
+        }
+
+        public void PrintWithDelay(string text)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(delayMilliseconds);
+            }
+        }
+    }
+}
