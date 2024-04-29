ddb623dd8b35123c87332f711fe920ce7739651c
bananaNuggets75 <gazokenan@gmail.com>
2024-04-29 21:55:26 +0800

    added player class

---
+++ hospital_exploration/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hospital_escape
+{
+	public class Player
+	{
+        public bool HasKey { get; set; }
+
+        public Player()
+        {
+            HasKey = false;
+        }
+    }
+}
+
