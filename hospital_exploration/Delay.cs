d2a9aaa9420cf610985fe5fae6d272546aa35ebd
bananaNuggets75 <gazokenan@gmail.com>
2024-04-29 21:13:59 +0800

    fix issue entering

--- hospital_exploration/Delay.cs
+++ hospital_exploration/Delay.cs
@@ -5,16 +5,22 @@ namespace hospital_escape
 {
     public class Delay
     {
-        
         public void PrintWithDelay(string text, int delayMilliseconds)
         {
             foreach (char c in text)
             {
-                Console.Write(c);
-                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter)
+                if (Console.KeyAvailable)
                 {
-                    break;
+                    var key = Console.ReadKey(true).Key;
+                    if (key == ConsoleKey.Enter)
+                    {
+                        while (Console.KeyAvailable)
+                            Console.ReadKey(true);
+                        Console.Write(text.Substring(text.IndexOf(c)));
+                        return;
+                    }
                 }
+                Console.Write(c);
                 Thread.Sleep(delayMilliseconds);
             }
         }
