8504282e62f1c004f63fe1279ae0e539743b50e8
bananaNuggets75 <gazokenan@gmail.com>
2024-04-29 22:47:49 +0800

    added search room method

--- hospital_exploration/Room.cs
+++ hospital_exploration/Room.cs
@@ -1,17 +1,32 @@
 ﻿using System;
-using hospital_escape;
 
 namespace hospital_escape
 {
     public abstract class Room
     {
         protected Game game;
+        protected bool hasKey;
 
-        public Room(Game game)
+        public Room(Game game, bool hasKey)
         {
             this.game = game;
+            this.hasKey = hasKey;
         }
 
         public abstract void Enter();
+
+        public virtual void SearchRoom()
+        {
+            if (hasKey)
+            {
+                Console.WriteLine("You search the room and find a key hidden under the rug!");
+                hasKey = false;
+                game.Player.HasKey = true;
+            }
+            else
+            {
+                Console.WriteLine("You search the room but find nothing of interest.");
+            }
+        }
     }
 }
