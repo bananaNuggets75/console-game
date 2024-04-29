12140baa13fca33d086a900087db0c5bb2936d49
bananaNuggets75 <gazokenan@gmail.com>
2024-04-29 22:33:08 +0800

    update player has key

--- hospital_exploration/Game.cs
+++ hospital_exploration/Game.cs
@@ -1,43 +1,52 @@
 ﻿using System;
-using hospital_escape;
 
 namespace hospital_escape
 {
-
     public class Game
     {
         private Room currentRoom;
         private Delay delayPrint = new Delay();
+        public Player Player { get; set; }
+
+        public Game()
+        {
+            Player = new Player();
+        }
 
         public void Start()
         {
             bool playAgain;
             do
             {
-                delayPrint.PrintWithDelay("Welcome to Hospital Escape! \n", 70);
-                delayPrint.PrintWithDelay("You wake up in the hospital with absolutely no memory of who you are or what happened. \n", 50);
+                delayPrint.PrintWithDelay("Welcome to Hospital Escape!", 70);
+                delayPrint.PrintWithDelay("You wake up in the hospital with absolutely no memory of who you are or what happened.", 50);
 
                 currentRoom = new HospitalRoom(this);
                 currentRoom.Enter();
 
-                bool InputValidation;
-                string playAgainChoice;
+                if (Player.HasKey)
+                {
+                    currentRoom = new HiddenKeyRoom(this);
+                    currentRoom.Enter();
+                }
 
+                bool inputValidation;
+                string playAgainChoice;
                 do
                 {
-                    delayPrint.PrintWithDelay("Do you want to play again? (yes/exit) \n", 50);
+                    delayPrint.PrintWithDelay("Do you want to play again? (yes/exit)", 50);
                     playAgainChoice = Console.ReadLine()?.ToLower() ?? "";
-                    InputValidation = playAgainChoice == "yes" || playAgainChoice == "exit";
-                    if (!InputValidation)
+                    inputValidation = playAgainChoice == "yes" || playAgainChoice == "exit";
+                    if (!inputValidation)
                     {
                         Console.WriteLine("Invalid input. Please enter 'yes' or 'exit'.");
                     }
-                } while (!InputValidation);
+                } while (!inputValidation);
 
                 playAgain = playAgainChoice == "yes";
             } while (playAgain);
 
-            delayPrint.PrintWithDelay("Thanks dumbass!", 60);
+            delayPrint.PrintWithDelay("Thanks for playing!", 60);
         }
 
         public void ChangeRoom(Room newRoom)
