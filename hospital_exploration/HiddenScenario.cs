dc38b22417f4e25f9bb6bd58fb1a496d350a7e1d
bananaNuggets75 <gazokenan@gmail.com>
2024-05-01 14:26:30 +0800

    intiial logic hidden scenario

--- hospital_exploration/HiddenScenario.cs
+++ hospital_exploration/HiddenScenario.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+
 namespace hospital_escape
 {
-	public class HiddenScenario
-	{
-		public HiddenScenario()
-		{
-		}
-	}
+    public class HiddenScenario
+    {
+        private Random random = new Random();
+        private Delay delayPrint = new Delay();
+       
+        public void TriggerHiddenScenario(Player player)
+        {
+            int chance = random.Next(1, 10); 
+            
+            if (chance <= 90) 
+            { 
+                delayPrint.PrintWithDelay("You stumble upon a hidden room!", 50);
+            }
+        }
+    }
 }
-
