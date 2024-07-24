a68f0d7ef0fd0056c8ff3b439812357a6ee78921
bananaNuggets75 <gazokenan@gmail.com>
2024-07-24 23:18:21 +0800

    puzzle class

---
+++ hospital_exploration/Puzzle.cs
@@ -0,0 +1,27 @@
+﻿namespace hospital_escape
+{
+    public abstract class Puzzle
+    {
+        public abstract bool Solve(Player player);
+    }
+
+    public class FindKeyPuzzle : Puzzle
+    {
+        public override bool Solve(Player player)
+        {
+            Console.WriteLine("You see a key hidden in the corner of the room. Do you pick it up? (yes/no)");
+            string input = Console.ReadLine()?.ToLower() ?? "";
+            if (input == "yes")
+            {
+                player.AddItem("Key");
+                Console.WriteLine("You picked up the key!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("You decided not to pick up the key.");
+                return false;
+            }
+        }
+    }
+}
